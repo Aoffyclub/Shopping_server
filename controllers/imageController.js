0ded67372ba4397599d1cc4c3ba292b1afcae2d0
Aoffy <offfyclub@gmail.com>
2024-07-26 14:57:24 +0700

    update server base url

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -1,8 +1,8 @@
-
+require('dotenv').config()
 exports.image = async (req, res) => {
 
     res.json({
         success: 1,
-        image_url: `http://localhost:${process.env.port}/images/${req.file.filename}`,
+        image_url: `http://45.154.26.220:3000/images/${req.file.filename}`,
     });
 };
